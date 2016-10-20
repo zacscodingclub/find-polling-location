abf8ad5b2954733ae8548d153243d44b5bb8eb54
zacscodingclub <zbaston@gmail.com>
2016-10-20 14:28:13 -0400

    update #gps-message upon callback completion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -40,6 +40,7 @@ function getGeoLocation() {
 function setGeoCookie(position) {
   var cookie_val = position.coords.latitude + "|" + position.coords.longitude;
   document.cookie = "lat_lng=" + escape(cookie_val);
+  $('#gps-message').text("Go forth and vote!");
   enableSubmit();
 }
 
